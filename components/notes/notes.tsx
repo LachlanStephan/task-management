e4aee6dbdb87a3556a376f7bcf904c805239da0a
LachlanStephan <ljstephan116@gmail.com>
2022-06-27 21:07:35 +1000

    fixed comments

--- components/notes/notes.tsx
+++ components/notes/notes.tsx
@@ -1,6 +1,7 @@
 import { FC, useState, useEffect } from "react";
 import styles from "./Notes.module.css";
 import todoStyles from "../todos/Todos.module.css";
+
 interface Props {
   title: string;
 }
@@ -8,7 +9,7 @@ interface Props {
 interface Note {
   id: number;
   content: string;
-} 
+}
 
 const Notes: FC<Props> = (Props) => {
   const [notes, setNotes] = useState<Note[]>([]);
@@ -18,7 +19,7 @@ const Notes: FC<Props> = (Props) => {
   const newNote = () => {
     setCurrNote(undefined);
     setCurrText("");
-  }
+  };
 
   const addNote = () => {
     if (currNote) {
@@ -30,56 +31,60 @@ const Notes: FC<Props> = (Props) => {
     const newData: Note = {
       id: newId,
       content: currText,
-    }
-    const x = [... notes];
-    x.push(newData)
+    };
+
+    const x = [...notes];
+    x.push(newData);
     localStorage.setItem("notes", JSON.stringify(x));
     setNotes(x);
     setCurrNote(newData);
-  }
+  };
 
   const updateNote = (newText: string) => {
     if (currNote) {
       const updatedNote: Note = {
         id: currNote.id,
         content: newText,
-      }
-      setCurrNote(updatedNote)
-      const x = [... notes];
+      };
+
+      const x = [...notes];
+
       for (let i = 0; i < x.length; i++) {
         if (x[i].id === currNote.id) {
           x[i].content = currText;
         }
       }
+
+      setCurrNote(updatedNote);
       setNotes(x);
+
       localStorage.removeItem("notes");
       localStorage.setItem("notes", JSON.stringify(x));
     }
-  }
+  };
 
   const assignId = () => {
     const newId = notes.length + 1;
     return newId;
-  }
+  };
 
   const updateText = (e: any) => {
-    setCurrText(e.target.value)
-  }
+    setCurrText(e.target.value);
+  };
 
   // TODO: THIS
   const removeNote = (id: number) => {
     //
-  }
+  };
 
-  // this does not work - newCurr is corrext but setCurrNote is still the old note - not updating correctly | Might not have to worry - we will be setting a default at some point - may fix it????
   const selectNote = (target_id: number) => {
-    const newCurr = notes.find(ele => ele.id === target_id);
+    const newCurr = notes.find((ele) => ele.id === target_id);
 
     if (newCurr) {
-      setCurrNote(newCurr)
-      setCurrText(newCurr.content)
+      setCurrNote(newCurr);
+      setCurrText(newCurr.content);
     }
-  }
+  };
 
   const setInitialNote = () => {
     const initial = getLocal();
@@ -89,37 +94,43 @@ const Notes: FC<Props> = (Props) => {
     const curr: Note = {
       id: initial[0].id,
       content: initial[0].content,
-    }
+    };
 
     setCurrNote(curr);
     setCurrText(curr.content);
-  }
+  };
 
   const getLocal = () => {
-    const local = localStorage.getItem("notes"); 
+    const local = localStorage.getItem("notes");
 
     if (local) {
       return JSON.parse(local);
     }
     return [];
-  }
+  };
 
   // add this to li val.content
   const limitText = (title: string) => {
     //
-  }
+  };
 
   const addNoteWithEnter = (e: any) => {
     //
-  }
+  };
 
   useEffect(() => {
     setInitialNote();
-  }, [])
+  }, []);
 
-  const notesList = notes.map((val, i) => { 
+  const notesList = notes.map((val, i) => {
     return (
-      <li id={val.id.toString()} onClick={() => selectNote(val.id)} key={val.id}>{val.content}</li>
+      <li
+        id={val.id.toString()}
+        onClick={() => selectNote(val.id)}
+        key={i}
+      >
+        {val.content}
+      </li>
     );
   });
 
@@ -127,15 +138,23 @@ const Notes: FC<Props> = (Props) => {
     <aside className={styles.main}>
       <h1>{Props.title}</h1>
       <label className={todoStyles.label} htmlFor="addNote"></label>
-      <textarea value={currText} onChange={(e) => updateText(e)} className={styles.noteInput} id="addNote" placeholder="Note..." ></textarea>
+      <textarea
+        value={currText}
+        onChange={(e) => updateText(e)}
+        className={styles.noteInput}
+        id="addNote"
+        placeholder="Note..."
+      ></textarea>
 
       <div className={styles.buttonContainer}>
-        <button onClick={addNote} className={todoStyles.addBtn}>{currNote ? "Edit": "Go"}</button>
-        <button onClick={newNote} className={todoStyles.addBtn}>New</button>
+        <button onClick={addNote} className={todoStyles.addBtn}>
+          {currNote ? "Edit" : "Go"}
+        </button>
+        <button onClick={newNote} className={todoStyles.addBtn}>
+          New
+        </button>
       </div>
-    <ul>
-      {notesList}
-    </ul>
+      <ul>{notesList}</ul>
     </aside>
   );
 };
